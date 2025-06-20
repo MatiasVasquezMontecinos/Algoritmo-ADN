4012e69a4f071456f4c33ff58953007d3ab8dabd
Lukas Escobar <134743612+Lescobar2904@users.noreply.github.com>
2025-06-19 23:40:57 -0400

    cambio

--- crearcromosoma.py
+++ crearcromosoma.py
@@ -1,13 +1,13 @@
 import numpy as np
+import random
 
-def generar_poblacion_inicial(pob_max, N, pesos, quorum_min):
+def generar_poblacion_inicial(pob_max, n_diputados, quorum_q):
     poblacion = []
 
     while len(poblacion) < pob_max:
-        individuo = np.random.randint(0, 2, N)  # genera vector binario aleatorio
-        suma_votos = np.sum(individuo * pesos)
-
-        if suma_votos >= quorum_min:
-            poblacion.append(individuo)
+        indices = random.sample(range(n_diputados), quorum_q)  # q diputados únicos
+        individuo = np.zeros(n_diputados, dtype=int)
+        individuo[indices] = 1
+        poblacion.append(individuo)
 
     return np.array(poblacion)
